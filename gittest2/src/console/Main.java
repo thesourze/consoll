93ec7509b98ad4a506fceef3814abb80f7ae536a
thesourze <notawake@thesourze.eu>
2014-04-24 14:22:53 +0200

    upload 1

--- gittest2/src/console/Main.java
+++ gittest2/src/console/Main.java
@@ -1,15 +1,33 @@
 package console;
 
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.AsyncPlayerChatEvent;
 import org.bukkit.plugin.java.JavaPlugin;
  
-public final class Main extends JavaPlugin {
-    @Override
+public class Main extends JavaPlugin implements Listener  {
+    
+	@EventHandler
+	public void onPlayerChat(AsyncPlayerChatEvent e) {
+		for (String word : e.getMessage().split(" ")) {
+			if (getConfig().getString("badwords").contains(word)) {
+				e.setCancelled(true);
+				e.getPlayer().sendMessage(ChatColor.RED + "Dont Swear");
+			}
+		}
+	}
+	
     public void onEnable() {
-        // TODO Insert logic to be performed when the plugin is enabled
+    getConfig().options().copyDefaults(true);
+    saveConfig();
+    Bukkit.getServer().getPluginManager().registerEvents(this, this);
+    
     }
  
     @Override
     public void onDisable() {
-        // TODO Insert logic to be performed when the plugin is disabled
+    
     }
 }
